60fd241b1c22912ef534ec751e53c7f4db7427fd
Mauro Farias Brito <maurofabri@gmail.com>
2023-07-26 20:28:13 -0300

    [#1] Criando o projeto.

---
+++ VRBeneficios/src/main/java/br/com/cadmus/vrbeneficios/VrBeneficiosApplication.java
@@ -0,0 +1,13 @@
+package br.com.cadmus.vrbeneficios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrBeneficiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VrBeneficiosApplication.class, args);
+	}
+
+}
