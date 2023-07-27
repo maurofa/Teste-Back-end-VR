b765432abd4f0e141c047318bc6907aa45bc1219
Mauro Farias Brito <mauro.brito@mpdft.mp.br>
2023-07-27 15:29:36 -0300

    [#1] Configuração do Banco.

---
+++ VRBeneficios/src/main/java/br/com/cadmus/vrbeneficios/config/JpaConfiguration.java
@@ -0,0 +1,51 @@
+package br.com.cadmus.vrbeneficios.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import jakarta.persistence.EntityManagerFactory;
+
+@Configuration
+@EnableJpaRepositories("br.com.cadmus.vrbeneficios.entity")
+public class JpaConfiguration {
+
+	@Bean
+	DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/miniautorizador?serverTimezone=UTC");
+		dataSource.setUsername("root");
+		return dataSource;
+	}
+
+	@Bean
+	JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+		return new JpaTransactionManager(emf);
+	}
+
+	@Bean
+	JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
+		jpaVendorAdapter.setDatabase(Database.MYSQL);
+		jpaVendorAdapter.setGenerateDdl(true);
+		return jpaVendorAdapter;
+	}
+
+	@Bean
+	LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean lemfb = new LocalContainerEntityManagerFactoryBean();
+		lemfb.setDataSource(dataSource());
+		lemfb.setJpaVendorAdapter(jpaVendorAdapter());
+		lemfb.setPackagesToScan("br.com.cadmus.vrbeneficios.entity");
+		return lemfb;
+	}
+}
